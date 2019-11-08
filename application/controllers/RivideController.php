875fd4283b8921870e7306b85aa7bbc95998cf0a
Ahmad Faizal <ahmadfaizaaal@gmail.com>
2019-11-08 08:29:33 +0700

    Developing ritment login page

--- application/controllers/RivideController.php
+++ application/controllers/RivideController.php
@@ -1,17 +1,22 @@
-<?php 
-class RivideController extends CI_Controller {
+<?php
+class RivideController extends CI_Controller
+{
 
     public function index()
     {
-        $this->load->helper('url');
+        // $this->load->helper('url');
         $this->load->view('beranda');
     }
 
     public function rivide()
     {
-        $this->load->helper('url');
+        // $this->load->helper('url');
         $this->load->view('rivide/dashboard');
     }
 
+    public function login()
+    {
+        $data['title'] = 'Login - Ritment';
+        $this->load->view('login', $data);
+    }
 }
-?>
